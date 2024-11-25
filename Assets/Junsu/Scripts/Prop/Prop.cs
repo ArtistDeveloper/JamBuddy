2f946c623ae76502afa7b6f95f338c37d5e72a10
JunsuPark <somadubel@gmail.com>
2024-11-25 09:45:46 +0900

    [Feat] 피격 VFX 한 개 추가

--- Assets/Junsu/Scripts/Prop/Prop.cs
+++ Assets/Junsu/Scripts/Prop/Prop.cs
@@ -9,6 +9,7 @@ namespace Jambuddy.Junsu
         public bool isRotation;
         public int opposite_damage = 10;
         public int rotation_damage = 10;
+        public GameObject vfxPrefab; // Inspector에서 이 프리팹을 할당
 
         private void OnCollisionEnter(Collision other)
         {
@@ -16,12 +17,32 @@ namespace Jambuddy.Junsu
             {
                 if (isOppositeMoving)
                 {
+                    GameObject vfxInstance = Instantiate(vfxPrefab, other.transform.position, Quaternion.identity);
+
+                    Animator animator = vfxInstance.GetComponent<Animator>();
+                    if (animator != null)
+                    {
+                        animator.Play("OppositeAnim");
+                    }
+                    
+                    Destroy(vfxInstance, 1.0f); // 애니메이션 길이에 맞춰서 조정
+
                     other.transform.GetComponent<Monster>().TakeDamage(opposite_damage);
                 }
 
                 if (isRotation)
                 {
                     other.transform.GetComponent<Monster>().TakeDamage(rotation_damage);
+
+                    GameObject vfxInstance = Instantiate(vfxPrefab, other.transform.position, Quaternion.identity);
+
+                    Animator animator = vfxInstance.GetComponent<Animator>();
+                    if (animator != null)
+                    {
+                        animator.Play("OppositeAnim");
+                    }
+
+                    Destroy(vfxInstance, 1.0f); // 애니메이션 길이에 맞춰서 조정
                 }
             }
         }
